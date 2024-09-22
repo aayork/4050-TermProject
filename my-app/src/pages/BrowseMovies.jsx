d47fc0248c330129eb6efaf2ed9b412eef2ebe6e
Aidan York <113198662+aayork@users.noreply.github.com>
2024-09-22 18:57:08 -0400

    grid chanegs

--- my-app/src/pages/BrowseMovies.jsx
+++ my-app/src/pages/BrowseMovies.jsx
@@ -2,7 +2,10 @@ import { Card } from "../components/Card";
 
 export function BrowseMovies() {
   return (
-    <div className="grid-flow-col-dense">
+    <div className="grid grid-cols-[repeat(auto-fit,minmax(375px,1fr))] gap-2">
+      <Card />
+      <Card />
+      <Card />
       <Card />
     </div>
   );
