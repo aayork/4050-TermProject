3e2fb9caaa10a1edae3b99a36efa30ba9f9bdb81
Will Gresham <willgresham34@gmail.com>
2024-09-24 21:40:41 -0400

    admin page started

--- my-app/src/App.jsx
+++ my-app/src/App.jsx
@@ -5,6 +5,7 @@ import { BrowseMovies } from "./pages/BrowseMovies";
 import { HomePage } from "./pages/HomePage";
 import { Login } from "./pages/Login";
 import { CreateAcc } from "./pages/CreateAcc";
+import { Admin } from "./pages/Admin";
 
 function App() {
   return (
@@ -17,6 +18,7 @@ function App() {
           <Route path="/login" element={<Login />} />
           <Route path="/login/:key" element={<Login />} />
           <Route path="/createAccount" element={<CreateAcc />} />
+          <Route path="/admin" element={<Admin />} />
         </Routes>
       </Router>
     </>
