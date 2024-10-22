102b6b40b94c47233faeeeee647c2b528992d3d2
Aidan York <113198662+aayork@users.noreply.github.com>
2024-10-22 16:36:00 -0400

    remove lines 8 & 9 from apps.py

--- CinemaProject/CinemaApp/apps.py
+++ CinemaProject/CinemaApp/apps.py
@@ -4,6 +4,3 @@ from django.apps import AppConfig
 class CinemaappConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'CinemaApp'
-
-    def ready(self):
-        import CinemaApp.signals
