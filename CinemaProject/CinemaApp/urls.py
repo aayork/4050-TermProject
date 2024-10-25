ffefbe073f866d426535df5e86c744cf669bd8cf
prestonbrown <brownpreston2490@gmail.com>
2024-10-24 20:15:34 -0400

    Encrypted Payment info, add and update promotion

--- CinemaProject/CinemaApp/urls.py
+++ CinemaProject/CinemaApp/urls.py
@@ -2,12 +2,11 @@ from django.urls import path
 from django.contrib.auth import views as auth_views
 from .views import MovieListView, MovieDetailView, PromotionDetailView, GetAllProfiles, MovieCreateView, MovieUpdateView, MovieDeleteView
 from .views import (DeletePaymentView, UserPaymentView, AddPaymentView,
-                    AddAddressView, DeleteAddressView, UserAddressView)
+                    AddAddressView, DeleteAddressView, UserAddressView, AddPromotionView, UpdatePromotionView)
 
 urlpatterns = [
     path('getMovies/', MovieListView.as_view(), name='movie-list'),
     path('getMovieDetails/<int:pk>/', MovieDetailView.as_view(), name='movie-detail'),
-    path('getPromotions/', PromotionDetailView.as_view(), name='promotion-details'),
     path('createMovie/', MovieCreateView.as_view(), name='movie-create'),
     path('updateMovie/<int:id>/', MovieUpdateView.as_view(), name='movie-update'),
     path('deleteMovie/<int:id>/', MovieDeleteView.as_view(), name='movie-delete'),
@@ -20,5 +19,9 @@ urlpatterns = [
     path('user/address/add/', AddAddressView.as_view(), name='address-add'),
     path('user/address/delete/<int:id>/', DeleteAddressView.as_view(), name='address-delete'),
     path('user/address/<int:user_id>/', UserAddressView.as_view(), name='address-view'),
+
+    path('getPromotions/', PromotionDetailView.as_view(), name='promotion-details'),
+    path('promotion/add/', AddPromotionView.as_view(), name='promotion-add'),
+    path('promotion/update/<int:pk>/', UpdatePromotionView.as_view(), name='promotion-update'),
 ]
 
