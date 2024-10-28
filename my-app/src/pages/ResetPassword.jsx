861ce74b46261300ed6c2107b096dae183c6628f
Aidan York <113198662+aayork@users.noreply.github.com>
2024-10-28 15:40:44 -0400

    reorganize reset pw page

--- my-app/src/pages/ResetPassword.jsx
+++ my-app/src/pages/ResetPassword.jsx
@@ -22,8 +22,8 @@ export function ResetPassword() {
   };
 
   return (
-    <div className="p-2">
-      <h2>Forgot Password</h2>
+    <div className="m-4">
+      <h1 className="text-3xl font-bold mb-2">Forgot Password</h1>
       <div className="inline-flex">
         <form onSubmit={handleFormSubmit}>
           <input
@@ -35,7 +35,7 @@ export function ResetPassword() {
             className="mb-2 p-2 border rounded"
           />
           <button
-            className="group flex h-min m-8 items-center disabled:opacity-50 disabled:hover:opacity-50 hover:translate-y-1 transition-transform justify-center ring-none rounded-lg shadow-lg font-semibold py-1 px-2 font-dm focus:outline-none focus-visible:outline-2 focus-visible:outline-offset-2 bg-violet-500 border-b-violet-700 disabled:border-0 disabled:bg-violet-500 disabled:text-white ring-white text-white border-b-4 hover:border-0 active:border-0 hover:text-gray-100 active:bg-violet-800 active:text-gray-300 focus-visible:outline-monkey-green text-sm sm:text-base dark:bg-green-800 dark:border-gray-700 dark:border-b-green-950"
+            className="h-min m-4 items-center disabled:opacity-50 disabled:hover:opacity-50 hover:translate-y-1 transition-transform justify-center ring-none rounded-lg shadow-lg font-semibold py-1 px-2 font-dm focus:outline-none focus-visible:outline-2 focus-visible:outline-offset-2 bg-violet-500 border-b-violet-700 disabled:border-0 disabled:bg-violet-500 disabled:text-white ring-white text-white border-b-4 hover:border-0 active:border-0 hover:text-gray-100 active:bg-violet-800 active:text-gray-300 focus-visible:outline-monkey-green text-sm sm:text-base dark:bg-green-800 dark:border-gray-700 dark:border-b-green-950"
             type="submit"
           >
             Send Reset Link
