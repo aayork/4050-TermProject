fd3409c62414156f9e47c80973011de4c6a28b91
Aidan York <113198662+aayork@users.noreply.github.com>
2024-11-18 14:38:13 -0500

    fix card spacing

--- my-app/src/pages/HomePage.jsx
+++ my-app/src/pages/HomePage.jsx
@@ -73,7 +73,7 @@ export function HomePage() {
                 (movie) =>
                   movie.movieName
                     .toLowerCase()
-                    .includes(searchTerm.toLowerCase()) && movie.is_active
+                    .includes(searchTerm.toLowerCase()) && movie.is_active,
               )
               .map((movie) => (
                 <MovieCard key={movie.movieName} movie={movie} />
@@ -82,13 +82,13 @@ export function HomePage() {
           <div>
             <h3 className="font-semibold text-xl">Coming Soon...</h3>
           </div>
-          <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-5 lg:grid-cols-auto-fit gap-4">
+          <div className="grid xs:grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 gap-4">
             {movies
               .filter(
                 (movie) =>
                   movie.movieName
                     .toLowerCase()
-                    .includes(searchTerm.toLowerCase()) && !movie.is_active
+                    .includes(searchTerm.toLowerCase()) && !movie.is_active,
               )
               .map((movie) => (
                 <MovieCard key={movie.movieName} movie={movie} />
